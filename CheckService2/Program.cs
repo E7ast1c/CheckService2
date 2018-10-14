17fd01a36bc74a1e481a66f067877953fcc70065
E7ast1c <ipalekh@yandex.ru>
2018-10-14 12:10:22 +0300

    Добавьте файлы проекта.

---
+++ CheckService2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Net.Mail;
+using NLog;
+
+namespace CheckService2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i=0; ;i++ )
+            {
+                GetService("Service1", "Server1");
+                GetService("Service2", "Server2");
+                GetService("Service3", "Server3");
+                GetService("Service4", "Server4");
+            }
+        }
+
+        static void GetService (string ServiceName,string ComputerName)
+        {
+
+            int CheckServiceTimer = 3000; // период срабатывания таймера на опрос сервиса
+            logger.Trace("Период срабатывания таймера на опрос сервиса = " + CheckServiceTimer);
+            try
+            {
+                ServiceController[] scServices;
+                scServices = ServiceController.GetServices();
+
+                
+                foreach (ServiceController scTemp in scServices)
+                {
+                    ServiceController sc = new ServiceController(ServiceName, ComputerName);
+                    Thread.Sleep(CheckServiceTimer); // сам таймер на задержку
+                    if (sc.Status == ServiceControllerStatus.Running)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        logger.Info (ComputerName + " (" + sc.DisplayName + ")" + " Status = " + sc.Status);
+                        Console.ResetColor();
+                        break;
+                    }
+                    //else if (sc.Status == ServiceControllerStatus.StopPending)
+                    //{
+                    //    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    //    Console.WriteLine (DateTime.Now.ToString() + " " + ComputerName + " (" + sc.DisplayName + ")" + " Status = " + sc.Status);
+                    //    Console.ResetColor();
+                    //    break;
+                    //}
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        logger.Info (ComputerName + " (" + sc.DisplayName + ")" + " Status = " + sc.Status);
+                        Console.ResetColor();
+                        SmtpMail.SendEmail("example@gmail.com",
+                            "example@gmail.com",
+                            sc.Status + " " + sc.DisplayName + " on server " + ComputerName,
+                            "<h4>" + sc.Status + " " + sc.DisplayName + " on server " + ComputerName + "</h4>",
+                            "smtp.gmail.com",
+                            587,
+                            "Password");
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                logger.Error(DateTime.Now.ToString() + " " + ex.Message);
+               // Console.WriteLine (DateTime.Now.ToString() + " " + ex.Message);
+                Console.ResetColor();
+            }
+        }
+
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+       
+    }
+}
---
+++ CheckService2/SMTPMail.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using NLog;
+
+namespace CheckService2
+{
+    class SmtpMail
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public static void SendEmail (string from, string to, string Subject, string Body, string Host, int Port, string Password)
+        {
+            try
+            {
+                logger.Debug("Инициирую отправку сообщения с параметрами: " + from + " "+ to + " " + Subject + " " + Body + " " + Host + " " + Port + " " + "Password");
+                MailAddress fromMailAddress = new MailAddress(from, "CheckService");
+                if (fromMailAddress != null)
+                {
+                    logger.Debug("Переменной fromMailAddress передано значение  = " + fromMailAddress);
+                }
+                else
+                {
+                    logger.Error("Переменной fromMailAddress передано значение  = " + fromMailAddress);
+                }
+                MailAddress toAddress = new MailAddress(to, "Recepient");
+                if (toAddress != null)
+                {
+                    logger.Debug("Переменной toAddress передано не корректное значение = " + toAddress);
+                }
+                else
+                {
+                    logger.Error("Переменной toAddress передано не корректное значение = " + toAddress);
+                }
+                using (MailMessage mailMessage = new MailMessage(from, to))
+                using (SmtpClient smtpClient = new SmtpClient())
+                {
+                    mailMessage.Subject = Subject;
+                    mailMessage.Body = Body;
+                    mailMessage.IsBodyHtml = true;
+                    smtpClient.Host = Host;
+                    smtpClient.Port = Port;
+                    smtpClient.EnableSsl = true;
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Credentials = new NetworkCredential(fromMailAddress.Address, Password);
+
+                    smtpClient.Send(mailMessage);
+
+                    logger.Info("Сообщение от " + fromMailAddress + " отправлено на адрес " + toAddress);
+                }
+            }
+            catch
+            {
+                Exception ex = new Exception();
+                Console.ForegroundColor = ConsoleColor.Red;
+                logger.Error("При отправке сообщения произвошла ошибка " + ex.Message + " " + ex.HResult + " " + ex.Data);
+                Console.ResetColor();
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
